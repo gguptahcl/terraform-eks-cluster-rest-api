dd8f3661a62811123adc11f0c06bb1525d943ab1
Gaurav Gupta <g.gupta@hcl.com>
2018-10-29 00:18:36 -0700

    Add Auto scaling Policy

--- modules/eks/eks-worker-nodes.tf
+++ modules/eks/eks-worker-nodes.tf
@@ -171,7 +171,8 @@ resource "aws_autoscaling_policy" "demo" {
   scaling_adjustment     = 1
   adjustment_type        = "ChangeInCapacity"
   policy_type	= "TargetTrackingScaling"
-  cooldown               = 300
+
+  #cooldown               = 300
   autoscaling_group_name = "${aws_autoscaling_group.demo.name}"
   
 target_tracking_configuration {
